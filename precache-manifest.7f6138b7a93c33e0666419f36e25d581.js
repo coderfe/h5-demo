68000c71e792ff9a6d6837023264588bd4556718
coderfee <coderfee@outlook.com>
2020-04-25 19:45:13 +0800

    Updates

---
+++ precache-manifest.7f6138b7a93c33e0666419f36e25d581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea634e7009a282fad0aa8c7a37448a10",
+    "url": "/h5-demo/index.html"
+  },
+  {
+    "revision": "ea26046246a10e786d34",
+    "url": "/h5-demo/static/css/main.9c49039f.chunk.css"
+  },
+  {
+    "revision": "6fb055b87b16b647e64d",
+    "url": "/h5-demo/static/js/2.8dbc6f34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/h5-demo/static/js/2.8dbc6f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea26046246a10e786d34",
+    "url": "/h5-demo/static/js/main.03ca8e92.chunk.js"
+  },
+  {
+    "revision": "42e05b1c84945194a8ab",
+    "url": "/h5-demo/static/js/runtime-main.258c157b.js"
+  },
+  {
+    "revision": "d52905c4c29579a19913bbbf65f58367",
+    "url": "/h5-demo/static/media/moonlight.d52905c4.jpg"
+  }
+]);
